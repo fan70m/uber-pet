ce34c136b95f7e8b427ac691a6c52effec0f3237
馬帥 <mashuai@Marshalls-MAC.local>
2019-04-03 16:22:29 +0800

    add and update table

--- sql/index.js
+++ sql/index.js
@@ -13,7 +13,32 @@ sql.query = {
 
 	// Update
 	update_info: 'UPDATE Users SET first_name=$2, last_name=$3 WHERE username=$1',
-  update_pass: 'UPDATE Users SET password=$2 WHERE username=$1',
+    update_pass: 'UPDATE Users SET password=$2 WHERE username=$1',
+
+    //insert area
+    add_area: 'INSERT INTO Areas(areaid, areaname) VALUES($1,$2)',
+    //update area
+    update_area: 'UPDATE Areas SET areaname=$2 WHERE username=$1',
+
+    //insert pet
+    add_pet: 'INSERT INTO Pets(petid, petname, type, starttime, endtime) VALUES($1,$2,$3,$4,$5)',
+    update_area: 'UPDATE Pets SET petname=$2, type=$3, starttime=$4, endtime=$5 WHERE username=$1',
+
+    //new appointment
+    add_appointment: 'INSERT INTO appointment(appointmentid, starttime, endtime) VALUES($1,$2,$3)',
+
+    //new rate
+    add_rate: 'INSERT INTO Rate(appointmentid, userid, comment) VALUES($1,$2,$3)',
+
+    //new payment
+    add_payment: 'INSERT INTO Payment(paymentid, credit) VALUES($1,$2)',
+
+    //new account
+    add_account: 'INSERT INTO Accounts(accountid, balance, userid) VALUES($1,$2,$3)',
+
+
+
+
 
 
 }
